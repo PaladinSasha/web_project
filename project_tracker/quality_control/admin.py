c27f3229cdb63bc74a83d7253a5d4a314cbea159
Alex <al_mezh@mail.ru>
2024-05-04 01:04:30 +0300

    Настроил панель администратора

--- project_tracker/quality_control/admin.py
+++ project_tracker/quality_control/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import BugReport, FeatureRequest
 
-# Register your models here.
+
+@admin.register(BugReport)
+class BugAdmin(admin.ModelAdmin):
+    fieldsets = [
+            ('Main Information', {'fields': ['title', 'description', 'project', 'task']}),
+            ('Additional Information', {'fields': ['status', 'priority']}),
+        ]
+
+    list_display = ('title', 'project', 'task', 'status', 'priority', 'created_at', 'updated_at')
+    list_filter = ('task', 'status', 'priority', 'updated_at')
+    search_fields = ('title', 'description')
+    readonly_fields = ('created_at', 'updated_at')
+    list_editable = ('status',)
+
+
+@admin.register(FeatureRequest)
+class FeatureAdmin(admin.ModelAdmin):
+    fieldsets = [
+            ('Main Information', {'fields': ['title', 'description', 'project', 'task']}),
+            ('Additional Information', {'fields': ['status', 'priority']}),
+        ]
+    list_display = ('title', 'project', 'task', 'status', 'priority', 'created_at', 'updated_at')
+    list_filter = ('task', 'status', 'priority', 'updated_at')
+    search_fields = ('title', 'description')
+    readonly_fields = ('created_at', 'updated_at')
+    list_editable = ('status',)
