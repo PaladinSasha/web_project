cde43ea2fbc2c5a8aee6f8bf5b196bcdbe849d04
Alex <al_mezh@mail.ru>
2024-05-05 23:59:34 +0300

    Добавил шаблоны для контроля качества и переписал view

--- project_tracker/quality_control/views.py
+++ project_tracker/quality_control/views.py
@@ -3,70 +3,44 @@ from django.urls import reverse
 from django.views import View
 from django.views.generic import DetailView
 from quality_control.models import BugReport, FeatureRequest
+from django.shortcuts import render, get_object_or_404
 
 
 def index(request):
-    bug_list_url = reverse('quality_control:bug_list')
-    feature_list_url = reverse('quality_control:feature_list')
-    html = (f"<h1>Система контроля качества</h1><a href='{bug_list_url}'>Список всех багов</a><br>"
-            f"<a href='{feature_list_url}'>Запросы на улучшение</a>")
-    return HttpResponse(html)
+    return render(request, template_name='quality_control/index.html')
 
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
-        bug_list_url = reverse('quality_control:bug_list')
-        feature_list_url = reverse('quality_control:feature_list')
-        html = (f"<h1>Система контроля качества</h1><a href='{bug_list_url}'>Список всех багов</a><br>"
-                f"<a href='{feature_list_url}'>Запросы на улучшение</a>")
-        return HttpResponse(html)
+        return render(request, 'quality_control/index.html')
 
 
 def bug_list(request):
     bugs = BugReport.objects.all()
-    bugs_html = f"<h1>Cписок отчетов об ошибках</h1><ul>"
-    for bug in bugs:
-        bugs_html += f"<li><a href={bug.id}>{bug.title}</a>, status={bug.status}</li>"
-    bugs_html += "</ul>"
-    return HttpResponse(bugs_html)
+    return render(request, 'quality_control/bug_list.html', {'bug_list': bugs})
 
 
 def feature_list(request):
     features = FeatureRequest.objects.all()
-    feature_html = f"<h1>Cписок запросов на улучшение</h1><ul>"
-    for feature in features:
-        feature_html += f"<li><a href={feature.id}>{feature.title}</a>, status={feature.status}</li>"
-    feature_html += "</ul>"
-    return HttpResponse(feature_html)
+    return render(request, 'quality_control/feature_list.html', {'feature_list': features})
 
 
 class BugDetailView(DetailView):
     model = BugReport
     pk_url_kwarg = 'bug_id'
-
-    def get(self, request, *args, **kwargs):
-        bug = self.get_object()
-        response_html = (f'<h1>{bug.title}</h1> <p>description: {bug.description}</p> <p>status={bug.status}</p>'
-                         f'task={bug.task}, project={bug.project}')
-        return HttpResponse(response_html)
+    template_name = 'quality_control/bug_detail.html'
 
 
 class FeatureDetailView(DetailView):
     model = FeatureRequest
     pk_url_kwarg = 'feature_id'
-
-    def get(self, request, *args, **kwargs):
-        feature = self.get_object()
-        response_html = (f'<h1>{feature.title}</h1> <p>description: {feature.description}</p> '
-                         f'<p>status={feature.status}</p><p>priority={feature.priority}</p>'
-                         f'task={feature.task}, '
-                         f'project=<a href=information_project/{feature.project.id}>{feature.project}</a>')
-        return HttpResponse(response_html)
+    template_name = 'quality_control/feature_detail.html'
 
 
 '''def feature_id_detail(request, feature_id):
     return HttpResponse(f'<h1>Детали улучшения {feature_id}</h1>')'''
 
 
-'''def bug_detail(request, bug_id):
-    return HttpResponse(f'<h1>Детали бага {bug_id}</h1>')'''
+def bug_detail(request, bug_id):
+    bug = get_object_or_404(BugReport, id=bug_id)
+    return render(request, 'quality_control/bug_detail.html', {'bug': bug})
--- project_tracker/tasks/urls.py
+++ project_tracker/tasks/urls.py
@@ -9,9 +9,9 @@ urlpatterns = [
     path('quality_control/', views_from_quality.index, name='quality_control'),
     # path('projects/', views.projects_list, name='projects_list'),
     # path('projects/<int:project_id>/', views.project_detail, name='project_detail'),
-    # path('projects/<int:project_id>/tasks/<int:task_id>/', views.task_details, name='task_detail'),
+    # path('projects/<int:project_id>/tasks/<int:task_id>/', views.task_detail, name='task_detail'),
 
-    path('', views.IndexView.as_view()),
+    path('', views.IndexView.as_view(), name='index'),
     path('projects/', views.ProjectListView.as_view(), name='projects_list'),
     path('projects/<int:project_id>/', views.ProjectDetailView.as_view(), name='project_detail'),
     path('projects/<int:project_id>/tasks/<int:task_id>/', views.TaskDetailView.as_view(), name='task_detail'),
